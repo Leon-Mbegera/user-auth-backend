1d6614742f9ae3eccf7b43fb85a06dfef8edc1ca
Leon-Mbegera <mbegeraleon89@gmail.com>
2025-09-24 12:50:03 +0300

    Add roles to the JWT to support RBAC

--- src/main/java/com/example/util/JwtUtil.java
+++ src/main/java/com/example/util/JwtUtil.java
@@ -1,12 +1,16 @@
 package com.example.util;
 
+import com.example.model.User;
+import com.example.repository.UserRepository;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
+import java.util.stream.Collectors;
 
 @Component
 public class JwtUtil {
@@ -15,9 +19,16 @@ public class JwtUtil {
     );
     private final long expirationMs = 86400000; // 1 day
 
+    @Autowired
+    private UserRepository userRepository;
+
     public String generateToken(String username) {
+        User user = userRepository.findByUsername(username).orElseThrow(() -> new RuntimeException("User not found."));
+
+        String roles = user.getRoles().stream().map(role -> role.getName()).collect(Collectors.joining(","));
         return Jwts.builder()
                 .subject(username)
+                .claim("roles", roles)
                 .issuedAt(new Date())
                 .expiration(new Date(System.currentTimeMillis() + expirationMs))
                 .signWith(secretKey, Jwts.SIG.HS512)
@@ -31,6 +42,15 @@ public class JwtUtil {
                 .parseSignedClaims(token)
                 .getPayload()
                 .getSubject();
+    }
+
+    public String getRolesFromToken(String token) {
+        return Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .get("roles", String.class);
     }
 
     public boolean validateToken(String token) {
