104cc1782706fbabc766a5b0e1bf3f42ced97074
Leon-Mbegera <mbegeraleon89@gmail.com>
2025-09-25 19:05:35 +0300

    controller action to submit a morgage application

---
+++ src/main/java/com/example/controller/ApplicationController.java
@@ -0,0 +1,29 @@
+package com.example.controller;
+
+import com.example.model.Application;
+import com.example.service.ApplicationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class ApplicationController {
+  @Autowired
+  private ApplicationService applicationService;
+
+  @PostMapping("/api/applications")
+  public ResponseEntity<Application> submitApplication(@RequestBody Map<String, String> body,
+      Authentication authentication) {
+
+    Application application = applicationService.submitApplication(authentication.getName(), body.get("fullName"),
+        body.get("address"), Double.parseDouble(body.get("loanAmount")), body.get("documentPath"));
+    return ResponseEntity.ok(application);
+  }
+
+
+
+}
